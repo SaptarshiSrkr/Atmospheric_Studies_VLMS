80a80249a8e9e5f977016569d4b88000e3568071
Saptarshi Sarkar <saptarshi5683@gmail.com>
2022-07-18 11:53:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Atmospheric_Studies_VLMS
+ Python code for estimation of elemental abundances of stars using MCMC
---
+++ Synthetic_Spec_Processing/Column_Reduction.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import os
+
+waverange = [6460,9000]
+
+print('\nStarting process to reduce raw synthetic spectrum files.')
+
+for file in os.listdir('../Data/SYNTHETIC/'):
+    filename = os.fsdecode(file)
+     
+    if filename.endswith(".7"): 
+         
+        df = pd.read_csv(f'../Data/SYNTHETIC/{filename}',usecols=[0, 1],names=['wave','flux'],sep='\s+',low_memory=False)
+        
+        if df.dtypes.wave == 'object':
+            df['wave']=df['wave'].str.replace('D','e')
+            df['wave']=df['wave'].astype(float)
+
+        df=df[(df['wave']>=waverange[0]) & (df['wave']<=waverange[1])]
+        df.reset_index(inplace = True, drop = True)
+
+        if df.dtypes.flux == 'object':
+            df['flux']=df['flux'].str.replace('D','e')
+            df['flux']=df['flux'].astype(float)
+
+        df['flux']=10**(df['flux']-8)      
+        
+        df.to_csv(f'../Data/Reduced/{filename}.txt',sep='\t',index=False,header=False)
+                
+        print(f'Spectrum {filename} reduced.')
+        continue
+    else:
+        continue
+
+print('\nProcess has been completed.')
