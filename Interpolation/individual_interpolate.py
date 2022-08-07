83ad70b69c107122f5f674e1cae51b905fb4a5ba
Saptarshi Sarkar <saptarshi5683@gmail.com>
2022-08-08 01:11:11 +0530

    interpolated results

--- Interpolation/individual_interpolate.py
+++ Interpolation/individual_interpolate.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from interpolation import spec_interpolate
 
-teff = 4045
-logg = 4.88
-metal = -1.33
+teff = 3961
+logg = 4.82
+metal = -1.39
 
 df = spec_interpolate(teff,logg,metal)
 filename = f'conv1.34_norm_lte{float(teff/100):.1f}-{logg:.2f}{metal:.1f}a+X.X.BT-Settl.CIFIST2011_2017.spec.7.txt'
